6c71007c7950afde65c30f3e19e7fa73bc06f6c1
stahldom <jeff@helpfulcomm.com>
2020-05-13 12:22:19 +0200

    MD-1243 individual posts are mobile friendly

--- src/components/Globals/Video.js
+++ src/components/Globals/Video.js
@@ -1,38 +1,40 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react"
+import styled from "styled-components"
+import { media } from "../../styles"
 
-const Video = ({url, title}) => {
-    return (
-        <BigFrame>
-        <Frame>
-            <iframe
-      src={url}
-      title={title}
-      allow="accelerometer; autoplay; encrypted-media; gyroscope;"
-      frameBorder="0"
-      webkitallowfullscreen="true"
-      mozallowfullscreen="true"
-      allowFullScreen
-      height="100%"
-
-
-    />
-        </Frame>
-        </BigFrame>
-    )
+const Video = ({ url, title }) => {
+  return (
+    <BigFrame>
+      <Frame>
+        <iframe
+          src={url}
+          title={title}
+          allow="accelerometer; autoplay; encrypted-media; gyroscope;"
+          frameBorder="0"
+          webkitallowfullscreen="true"
+          mozallowfullscreen="true"
+          allowFullScreen
+          height="100%"
+        />
+      </Frame>
+    </BigFrame>
+  )
 }
 
 export const Frame = styled.div`
-display: flex;
-flex: 1 1 auto;
-flex-direction: column;
-height: 488px;
-margin-top: 78px;
+  display: flex;
+  flex: 1 1 auto;
+  flex-direction: column;
+  margin-top: 78px;
+  height: 244px;
+  ${media.portraitTablet`height:366px;`}
+  ${media.tablet`height:488px;`}
+  ${media.laptop`height: 488px;`}
 `
 
 export const BigFrame = styled.div`
-display:flex;
-flex-direction: auto;
+  display: flex;
+  flex-direction: auto;
 `
 
 export default Video
--- src/components/Templates/Post-Template.styled.js
+++ src/components/Templates/Post-Template.styled.js
@@ -1,35 +1,34 @@
 import styled from "styled-components"
-import { setRem, setFlex, setColor, setTransition } from "../../styles"
-
+import { setRem, setFlex, setColor, setTransition, media } from "../../styles"
 
 export const Content = styled.div`
-font-size: ${setRem(18)};
-line-height: ${setRem(27)};
-font-weight: 300;
-color: ${setColor.mainBlack};
-h2 {
-  margin: ${setRem(40)} 0 ${setRem(10)} 0;
-}
-h3 { 
-  margin: ${setRem(40)} 0 ${setRem(10)} 0;
-}
-h4 {
-  margin: ${setRem(20)} 0;
-}
+  font-size: ${setRem(18)};
+  line-height: ${setRem(27)};
+  font-weight: 300;
+  color: ${setColor.mainBlack};
+  h2 {
+    margin: ${setRem(40)} 0 ${setRem(10)} 0;
+  }
+  h3 {
+    margin: ${setRem(40)} 0 ${setRem(10)} 0;
+  }
+  h4 {
+    margin: ${setRem(20)} 0;
+  }
 `
 
 export const Author = styled.div`
-padding: ${setRem(19)} ${setRem(0)};
-font-size: ${setRem(18)};
-line-height: ${setRem(27)};
-a {
-  text-decoration: none;
-  color: ${setColor.mainBlack};
-}
-a:hover {
-  color: ${setColor.secondaryColor};
-  ${setTransition}
-}
+  padding: ${setRem(19)} ${setRem(0)};
+  font-size: ${setRem(18)};
+  line-height: ${setRem(27)};
+  a {
+    text-decoration: none;
+    color: ${setColor.mainBlack};
+  }
+  a:hover {
+    color: ${setColor.secondaryColor};
+    ${setTransition}
+  }
 `
 
 export const FeaturedImage = styled.div`
@@ -37,14 +36,15 @@ export const FeaturedImage = styled.div`
   margin: ${setRem(76)} 0 0 0;
 `
 export const Grid = styled.div`
-  display: grid;
-  grid-template-columns: 2fr 6fr 2fr;
+  padding: 0 ${setRem(15)};
   h1 {
     padding: 0 0 4vh 0;
     margin: ${setRem(76)} 0 ${setRem(76)} ${setRem(0)};
     color: ${setColor.mainBlack};
-
   }
+  ${media.laptop`  display: grid;
+  grid-template-columns: 2fr 6fr 2fr;
+  `}
 `
 
 export const Date = styled.div`
@@ -52,72 +52,71 @@ export const Date = styled.div`
 `
 
 export const Meta = styled.div`
-display: flex;
-justify-content: space-between;
-margin-top: ${setRem(76)};
+  display: flex;
+  justify-content: space-between;
+  margin-top: ${setRem(76)};
 `
 
 export const Category = styled.div`
-font-size: ${setRem(16)};
-font-weight: 700;
-color: #686868;
-a {
+  font-size: ${setRem(16)};
+  font-weight: 700;
   color: #686868;
-  text-decoration: none;
-}
-a:hover {
-  color: ${setColor.secondaryColor};
-  ${setTransition}
-}
+  a {
+    color: #686868;
+    text-decoration: none;
+  }
+  a:hover {
+    color: ${setColor.secondaryColor};
+    ${setTransition}
+  }
 `
 
 export const Bottom = styled.div`
-display:flex;
-justify-content: space-between;
-margin: 0 0 ${setRem(76)};
+  display: flex;
+  justify-content: space-between;
+  margin: 0 0 ${setRem(76)};
 `
 
 export const Tag = styled.div`
-background: ${setColor.mainGrey};
-padding: ${setRem(7)} ${setRem(15)};
-margin: ${setRem()};
-border-radius: ${setRem()};
-}
-
+  background: ${setColor.mainGrey};
+  padding: ${setRem(7)} ${setRem(15)};
+  margin: ${setRem()};
+  border-radius: ${setRem()};
 `
+
 export const Tags = styled.div`
-a{
-  text-decoration: none;
-  color: ${setColor.mainBlack};
+  a {
+    text-decoration: none;
+    color: ${setColor.mainBlack};
+    ${setTransition}
+  }
+  a:hover {
+    color: ${setColor.mainWhite};
+  }
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  min-width: 10vw;
   ${setTransition}
-}
-a:hover {
-  color: ${setColor.mainWhite};
-}
-display: flex;
-justify-content: space-around;
-flex-wrap: wrap;
-min-width: 10vw;
-${setTransition}
 `
 export const Left = styled.div`
-display: flex;
-flex-direction: column;
+  display: flex;
+  flex-direction: column;
 `
 
 export const Social = styled.div`
-a {
-  color: ${setColor.mainBlack}
-}
-a:hover{
-  color: ${setColor.secondaryColor}
-}
-.icon {
-  margin: ${setRem(0)} ${setRem(5)};
-  ${setTransition}
-}
-.iconRight {
-  margin-right: ${setRem(5)};
-  ${setTransition}
-}
+  a {
+    color: ${setColor.mainBlack};
+  }
+  a:hover {
+    color: ${setColor.secondaryColor};
+  }
+  .icon {
+    margin: ${setRem(0)} ${setRem(5)};
+    ${setTransition}
+  }
+  .iconRight {
+    margin-right: ${setRem(5)};
+    ${setTransition}
+  }
 `
