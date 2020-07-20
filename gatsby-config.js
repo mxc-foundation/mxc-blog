da8d537a230835cd1c9a65c47b70a51ea55dd8f7
stahldom <jeff@stahlnecker.me>
2020-07-20 13:37:47 +0000

    MD-1447 added RSS for Korean, Chinese C and T

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,6 +38,38 @@ require("dotenv").config({
         `,
         feeds: [
           {
+            serialize: ({ query: { site, allStrapiKoPosts } }) => {
+              return allStrapiKoPosts.nodes.map(nodes => {
+                return Object.assign({}, nodes.post, {
+                  title: nodes.title,
+                  description: nodes.post.metaDescription,
+                  date: nodes.date,
+                  url: site.siteMetadata.siteUrl + `/ko/` + nodes.post.slug,
+                  guid: site.siteMetadata.siteUrl + `/ko/` + nodes.post.slug,
+                  custom_elements: [{ "content:encoded": nodes.post.content }],
+                })
+              })
+            },
+            query: `
+            {
+              allStrapiKoPosts(filter: {post: {publish: {eq: true}}}, sort: {fields: date, order: DESC}) {
+                nodes {
+                  date(formatString: "MMMM DD, YYYY")
+                  post {
+                    metaDescription
+                    slug
+                    content
+                  }
+                  title
+                }
+              }
+            }
+            
+            `,
+            output: "/ko/rss.xml",
+            title: "MXC Foundation RSS Feed",
+          },
+          {
             serialize: ({ query: { site, allStrapiPosts } }) => {
               return allStrapiPosts.nodes.map(nodes => {
                 return Object.assign({}, nodes.post, {
@@ -67,6 +99,70 @@ require("dotenv").config({
             
             `,
             output: "/rss.xml",
+            title: "MXC Foundation RSS Feed",
+          },
+          {
+            serialize: ({ query: { site, allStrapiZhtwPosts } }) => {
+              return allStrapiZhtwPosts.nodes.map(nodes => {
+                return Object.assign({}, nodes.post, {
+                  title: nodes.title,
+                  description: nodes.post.metaDescription,
+                  date: nodes.date,
+                  url: site.siteMetadata.siteUrl + `/zh-hant/` + nodes.post.slug,
+                  guid: site.siteMetadata.siteUrl + `/zh-hant` + nodes.post.slug,
+                  custom_elements: [{ "content:encoded": nodes.post.content }],
+                })
+              })
+            },
+            query: `
+            {
+              allStrapiZhtwPosts(filter: {post: {publish: {eq: true}}}, sort: {fields: date, order: DESC}) {
+                nodes {
+                  date(formatString: "MMMM DD, YYYY")
+                  post {
+                    metaDescription
+                    slug
+                    content
+                  }
+                  title
+                }
+              }
+            }
+            
+            `,
+            output: "/zh-hant/rss.xml",
+            title: "MXC Foundation RSS Feed",
+          },
+          {
+            serialize: ({ query: { site, allStrapiZhchPosts } }) => {
+              return allStrapiZhchPosts.nodes.map(nodes => {
+                return Object.assign({}, nodes.post, {
+                  title: nodes.title,
+                  description: nodes.post.metaDescription,
+                  date: nodes.date,
+                  url: site.siteMetadata.siteUrl + `/zh-hans/` + nodes.post.slug,
+                  guid: site.siteMetadata.siteUrl + `/zh-hans/` + nodes.post.slug,
+                  custom_elements: [{ "content:encoded": nodes.post.content }],
+                })
+              })
+            },
+            query: `
+            {
+              allStrapiZhchPosts(filter: {post: {publish: {eq: true}}}, sort: {fields: date, order: DESC}) {
+                nodes {
+                  date(formatString: "MMMM DD, YYYY")
+                  post {
+                    metaDescription
+                    slug
+                    content
+                  }
+                  title
+                }
+              }
+            }
+            
+            `,
+            output: "/zh-hans/rss.xml",
             title: "MXC Foundation RSS Feed",
           },
         ],
