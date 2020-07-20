5db3f4741797b62c2df4bcc05f61f926606a4376
stahldom <jeff@stahlnecker.me>
2020-07-20 10:38:08 +0000

    setting up for UI tests

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -61,6 +61,9 @@ const setResources = url.includes("/ko") ? KoResources : url.includes("/zh-hans"
           })}
         </Menu>
       </Column>
+      <Column>
+      This is a new column
+      </Column>
     </StyledFooter>
   )
 }
