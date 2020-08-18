649008a12395f1f30f31034d09c2b27799ca156d
NamGyeong <namgyeong.cho@mxc.org>
2020-08-18 18:12:56 +0200

    MD-1535 unified links

--- src/components/Constants/Links.js
+++ src/components/Constants/Links.js
@@ -1,22 +1,26 @@
-export default [
-  {
-    path: "https://www.mxc.org/",
-    text: "Home",
-  },
-  {
-    path: "https://www.mxc.org/mxc-token",
-    text: "Token",
-  },
-  {
-    path: "https://www.mxc.org/mxc-supernode",
-    text: "Supernode",
-  },
-  {
-    path: "/",
-    text: "Blog",
-  },
-  {
-    path: "https://www.mxc.org/about",
-    text: "About",
-  },
-]
+export function getTopMenu(lang) {
+  const token = lang === "en" ? "mxc-token": "token";
+  const Supernode = lang === "en" ? "mxc-supernode": "supernode";
+  return [
+    {
+      path: "https://www.mxc.org/",
+      text: "Home",
+    },
+    {
+      path: "https://www.mxc.org/"+token,
+      text: "Token",
+    },
+    {
+      path: "https://www.mxc.org/"+Supernode,
+      text: "Supernode",
+    },
+    {
+      path: "/"+lang,
+      text: "Blog",
+    },
+    {
+      path: "https://www.mxc.org/about",
+      text: "About",
+    },
+  ];
+}
