6457a2b88afe594f0499e546cbddd7d2b21b8337
stahldom <jeff@stahlnecker.me>
2020-08-25 15:34:58 +0200

    removed ternary string

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -7,203 +7,6 @@ import Categories from "./Categories"
 import PostRow from "./Globals/PostRow"
 import Category from "./Globals/Category"
 
-const Posts = () => {
-  const {
-    newsUpdate: { nodes: newsUpdate },
-    featured: { nodes: featured },
-    events: { nodes: events },
-    furtherReading: { nodes: furtherReading },
-    technology: { nodes: technology },
-    pressRelease: { nodes: pressRelease },
-    file: { childImageSharp: file },
-    useCase: { nodes: useCase },
-  } = useStaticQuery(getPosts)
-
-  return (
-    <Grid>
-      <div />
-      <FeaturedRow>
-        <Title>
-          <h1>Blog</h1>
-        </Title>
-        <Categories />
-        {featured.map(item => {
-          return (
-            <div key={item.id}>
-              <PostRow
-                heading={item.title}
-                text={item.post.metaDescription}
-                slug={item.post.slug}
-                image={
-                  item.featuredImage === null
-                    ? file.fluid
-                    : item.featuredImage === undefined
-                    ? file.fluid
-                    : item.featuredImage.childImageSharp.fluid
-                }
-                category={item.category.category}
-                date={item.date}
-                featured
-              />
-              <Line color={setColor.lightGrey} />
-            </div>
-          )
-        })}
-        <Category category="News Update" url="/categories/news-update">
-          {newsUpdate.map(data => {
-            return (
-              <div key={data.id}>
-                <PostRow
-                  heading={data.title}
-                  text={data.post.metaDescription}
-                  slug={data.post.slug}
-                  date={data.date}
-                  image={
-                    data.featuredImage !== null
-                      ? data.featuredImage.childImageSharp.fluid
-                      : file.fluid
-                  }
-                />
-                <Line color={setColor.lightGrey} />
-              </div>
-            )
-          })}
-        </Category>
-        <Category category="Events" url="/categories/events">
-          {events.map(data => {
-            return (
-              <div key={data.id}>
-                <PostRow
-                  heading={data.title}
-                  text={data.post.metaDescription}
-                  slug={data.post.slug}
-                  date={data.date}
-                  image={
-                    data.featuredImage !== null
-                      ? data.featuredImage.childImageSharp.fluid
-                      : file.fluid
-                  }
-                />
-                <Line color={setColor.lightGrey} />
-              </div>
-            )
-          })}
-        </Category>
-        <Category category="Further Reading" url="/categories/further-reading">
-          {furtherReading.map(data => {
-            return (
-              <div key={data.id}>
-                <PostRow
-                  heading={data.title}
-                  text={data.post.metaDescription}
-                  slug={data.post.slug}
-                  date={data.date}
-                  image={
-                    data.featuredImage !== null
-                      ? data.featuredImage.childImageSharp.fluid
-                      : file.fluid
-                  }
-                />
-                <Line color={setColor.lightGrey} />
-              </div>
-            )
-          })}
-        </Category>
-        <Category category="Press Releases" url="/categories/press-release">
-          {pressRelease.map(data => {
-            return (
-              <div key={data.id}>
-                <PostRow
-                  heading={data.title}
-                  text={data.post.metaDescription}
-                  slug={data.post.slug}
-                  date={data.date}
-                  image={
-                    data.featuredImage !== null
-                      ? data.featuredImage.childImageSharp.fluid
-                      : file.fluid
-                  }
-                />
-                <Line color={setColor.lightGrey} />
-              </div>
-            )
-          })}
-        </Category>
-        <Category category="Technology" url="/categories/technology">
-          {technology.map(data => {
-            return (
-              <div key={data.id}>
-                <PostRow
-                  heading={data.title}
-                  text={data.post.metaDescription}
-                  slug={data.post.slug}
-                  date={data.date}
-                  image={
-                    data.featuredImage !== null
-                      ? data.featuredImage.childImageSharp.fluid
-                      : file.fluid
-                  }
-                />
-                <Line color={setColor.lightGrey} />
-              </div>
-            )
-          })}
-        </Category>
-        <Category category="Use Cases" url="/categories/use-case">
-          {useCase.map(data => {
-            return (
-              <div key={data.id}>
-                <PostRow
-                  heading={data.title}
-                  text={data.post.metaDescription}
-                  slug={data.post.slug}
-                  date={data.date}
-                  image={
-                    data.featuredImage !== null
-                      ? data.featuredImage.childImageSharp.fluid
-                      : file.fluid
-                  }
-                />
-                <Line color={setColor.lightGrey} />
-              </div>
-            )
-          })}
-        </Category>
-      </FeaturedRow>
-      <div />
-    </Grid>
-  )
-}
-
-const Grid = styled.div`
-  ${media.tablet`  display: grid;
-  grid-template-columns: 10vw 80vw 10vw;
-  column-gap: ${setRem(5)};`};
-`
-
-const FeaturedRow = styled.div`
-  padding: 0 2vw;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  h1 {
-    text-align: center;
-    margin: 20px 0;
-    ${setFont}
-    font-size: ${setRem(38)};
-    color: ${setColor.mainBlack};
-  }
-`
-
-const Title = styled.div`
-  border-top: 3px solid ${setColor.mainBlack};
-  margin-top: ${setRem(40)};
-  ${media.tablet`
-    border-top: 3px solid ${setColor.mainBlack}; 
-    width: 5vw;
-    margin-top: ${setRem(80)};`};
-`
 const getPosts = graphql`
   {
     featured: allStrapiPosts(
@@ -407,6 +210,202 @@ const getPosts = graphql`
       }
     }
   }
+`
+
+const Posts = () => {
+  const {
+    newsUpdate: { nodes: newsUpdate },
+    featured: { nodes: featured },
+    events: { nodes: events },
+    furtherReading: { nodes: furtherReading },
+    technology: { nodes: technology },
+    pressRelease: { nodes: pressRelease },
+    file: { childImageSharp: file },
+    useCase: { nodes: useCase },
+  } = useStaticQuery(getPosts)
+
+  return (
+    <Grid>
+      <div />
+      <FeaturedRow>
+        <Title>
+          <h1>Blog</h1>
+        </Title>
+        <Categories />
+        {featured.map(item => {
+          return (
+            <div key={item.id}>
+              <PostRow
+                heading={item.title}
+                text={item.post.metaDescription}
+                slug={item.post.slug}
+                image={
+                  item.featuredImage?.childImageSharp.fluid
+                    ? item.featuredImage.childImageSharp.fluid
+                    : file.fluid
+                }
+                category={item.category.category}
+                date={item.date}
+                featured
+              />
+              <Line color={setColor.lightGrey} />
+            </div>
+          )
+        })}
+        <Category category="News Update" url="/categories/news-update">
+          {newsUpdate.map(data => {
+            return (
+              <div key={data.id}>
+                <PostRow
+                  heading={data.title}
+                  text={data.post.metaDescription}
+                  slug={data.post.slug}
+                  date={data.date}
+                  image={
+                    data.featuredImage !== null
+                      ? data.featuredImage.childImageSharp.fluid
+                      : file.fluid
+                  }
+                />
+                <Line color={setColor.lightGrey} />
+              </div>
+            )
+          })}
+        </Category>
+        <Category category="Events" url="/categories/events">
+          {events.map(data => {
+            return (
+              <div key={data.id}>
+                <PostRow
+                  heading={data.title}
+                  text={data.post.metaDescription}
+                  slug={data.post.slug}
+                  date={data.date}
+                  image={
+                    data.featuredImage !== null
+                      ? data.featuredImage.childImageSharp.fluid
+                      : file.fluid
+                  }
+                />
+                <Line color={setColor.lightGrey} />
+              </div>
+            )
+          })}
+        </Category>
+        <Category category="Further Reading" url="/categories/further-reading">
+          {furtherReading.map(data => {
+            return (
+              <div key={data.id}>
+                <PostRow
+                  heading={data.title}
+                  text={data.post.metaDescription}
+                  slug={data.post.slug}
+                  date={data.date}
+                  image={
+                    data.featuredImage !== null
+                      ? data.featuredImage.childImageSharp.fluid
+                      : file.fluid
+                  }
+                />
+                <Line color={setColor.lightGrey} />
+              </div>
+            )
+          })}
+        </Category>
+        <Category category="Press Releases" url="/categories/press-release">
+          {pressRelease.map(data => {
+            return (
+              <div key={data.id}>
+                <PostRow
+                  heading={data.title}
+                  text={data.post.metaDescription}
+                  slug={data.post.slug}
+                  date={data.date}
+                  image={
+                    data.featuredImage !== null
+                      ? data.featuredImage.childImageSharp.fluid
+                      : file.fluid
+                  }
+                />
+                <Line color={setColor.lightGrey} />
+              </div>
+            )
+          })}
+        </Category>
+        <Category category="Technology" url="/categories/technology">
+          {technology.map(data => {
+            return (
+              <div key={data.id}>
+                <PostRow
+                  heading={data.title}
+                  text={data.post.metaDescription}
+                  slug={data.post.slug}
+                  date={data.date}
+                  image={
+                    data.featuredImage !== null
+                      ? data.featuredImage.childImageSharp.fluid
+                      : file.fluid
+                  }
+                />
+                <Line color={setColor.lightGrey} />
+              </div>
+            )
+          })}
+        </Category>
+        <Category category="Use Cases" url="/categories/use-case">
+          {useCase.map(data => {
+            return (
+              <div key={data.id}>
+                <PostRow
+                  heading={data.title}
+                  text={data.post.metaDescription}
+                  slug={data.post.slug}
+                  date={data.date}
+                  image={
+                    data.featuredImage !== null
+                      ? data.featuredImage.childImageSharp.fluid
+                      : file.fluid
+                  }
+                />
+                <Line color={setColor.lightGrey} />
+              </div>
+            )
+          })}
+        </Category>
+      </FeaturedRow>
+      <div />
+    </Grid>
+  )
+}
+
+const Grid = styled.div`
+  ${media.tablet`  display: grid;
+  grid-template-columns: 10vw 80vw 10vw;
+  column-gap: ${setRem(5)};`};
+`
+
+const FeaturedRow = styled.div`
+  padding: 0 2vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  h1 {
+    text-align: center;
+    margin: 20px 0;
+    ${setFont}
+    font-size: ${setRem(38)};
+    color: ${setColor.mainBlack};
+  }
+`
+
+const Title = styled.div`
+  border-top: 3px solid ${setColor.mainBlack};
+  margin-top: ${setRem(40)};
+  ${media.tablet`
+    border-top: 3px solid ${setColor.mainBlack}; 
+    width: 5vw;
+    margin-top: ${setRem(80)};`};
 `
 
 export default Posts
