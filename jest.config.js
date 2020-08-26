7cba53de9bfeae78196dcd1eeb6b679cda22e59f
stahldom <jeff@stahlnecker.me>
2020-08-26 17:43:32 +0200

    add jest render test for categories component

--- jest.config.js
+++ jest.config.js
@@ -4,9 +4,17 @@ module.exports = {
   },
   moduleNameMapper: {
     ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
-    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.js`,
+    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/test/__mocks__/file-mock.js`,
   },
-  collectCoverageFrom: ["**/src/**/*.js"],
+  testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
+  transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
+  globals: {
+    __PATH_PREFIX__: ``,
+  },
+  testURL: `http://localhost`,
+  setupFiles: [`<rootDir>/test/loadershim.js`],
+  // projects: ["./test/jest.lint.js"],
+  collectCoverageFrom: ["**!/src/!**!/!*.js"],
   coverageThreshold: {
     global: {
       statements: 0,
@@ -15,14 +23,4 @@ module.exports = {
       lines: 0,
     },
   },
-  testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
-  transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
-  globals: {
-    __PATH_PREFIX__: ``,
-  },
-  testURL: `http://localhost`,
-  testEnvironment: "jest-environment-jsdom",
-  setupFiles: [`<rootDir>/test/loadershim.js`],
-  setupFilesAfterEnv: ["<rootDir>/test/setup-test-env.js"],
-  projects: ["./test/jest.lint.js"],
 }
--- src/components/__tests__/Posts.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import Posts from "../Posts"
-
-test('"Posts" renders', () => {
-  ;<Posts />
-})
